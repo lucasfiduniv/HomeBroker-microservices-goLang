b51aa47f3eb96a94f6043213df451172a3b039e4
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2023-12-09 19:47:34 -0300

    fazendo a asset e a order

---
+++ internal/market/entity/Order.go
@@ -0,0 +1,11 @@
+package entity
+
+type Order struct {
+	ID            string
+	Investor      *Investor
+	Asset         *Asset
+	Shares        int
+	PendingShares int
+	Price         float64
+	OrderType     string
+}
