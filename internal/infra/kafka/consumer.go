6dd5d0efe386deed8a880a191c08c922cdb7ece3
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2023-12-11 17:53:12 -0300

    fazendo o producer do kafka

--- internal/infra/kafka/consumer.go
+++ internal/infra/kafka/consumer.go
@@ -1,6 +1,8 @@
 package kafka
 
-import ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+import (
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+)
 
 type Consumer struct {
 	ConfigMap *ckafka.ConfigMap
@@ -13,19 +15,27 @@ func NewConsumer(configMap *ckafka.ConfigMap, topics []string) *Consumer {
 		Topics:    topics,
 	}
 }
+
 func (c *Consumer) Consume(msgChan chan *ckafka.Message) error {
 	consumer, err := ckafka.NewConsumer(c.ConfigMap)
 	if err != nil {
-		panic(err)
+		return err
 	}
+
 	err = consumer.SubscribeTopics(c.Topics, nil)
 	if err != nil {
-		panic(err)
+		consumer.Close() // Feche o consumidor se falhar ao se inscrever nos tópicos
+		return err
 	}
+
+	defer consumer.Close() // Certifique-se de fechar o consumidor quando a função retornar
+
 	for {
 		msg, err := consumer.ReadMessage(-1)
-		if err == nil {
-			msgChan <- msg
+		if err != nil {
+			return err // Retornar erro se ocorrer um problema ao ler a mensagem
 		}
+
+		msgChan <- msg
 	}
 }
