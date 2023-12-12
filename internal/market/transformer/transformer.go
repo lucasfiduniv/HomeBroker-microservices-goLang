23cf8e303a9f4d8e62faa7a4ced20d6a08baeaa9
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2023-12-12 09:48:12 -0300

    terminando o transformer

--- internal/market/transformer/transformer.go
+++ internal/market/transformer/transformer.go
@@ -5,4 +5,39 @@ import (
 	"github.com/lucasfiduniv/HomeBroker-microservices-goLang/internal/market/entity"
 )
 
-func TransformerInput(input dto.TradeInput) *entity.Order
+func TransformerInput(input dto.TradeInput) *entity.Order {
+	asset := entity.NewAsset(input.AssetId, input.AssetId, 1000)
+	investor := entity.NewInvestor(input.InvestorID)
+	order := entity.NewOrder(input.OrderID, asset, input.Shares, input.Price, input.OrderType)
+	if input.CurrentShares > 0 {
+		assetPosition := entity.NewInvestorAssetPosition(input.AssetId, input.CurrentShares)
+		investor.AddAssetPosition(assetPosition)
+	}
+	return order
+}
+
+func TranformOutput(order *entity.Order) *dto.OrderOutput {
+	output := &dto.OrderOutput{
+		OrderID:    order.ID,
+		InvestorID: order.Investor.ID,
+		AssetId:    order.Asset.ID,
+		OrderType:  order.OrderType,
+		Status:     order.Status,
+		Partial:    order.PendingShares,
+		Shares:     order.Shares,
+	}
+	var transactionsOutput []*dto.TransactionOutput
+	for _, t := range order.Transactions {
+		transactionsOutput := &dto.TransactionOutput{
+			TransactionID: t.ID,
+			BuyerID:       t.BuyingOrder.ID,
+			SellerID:      t.SellingOrder.ID,
+			AssetID:       t.SellingOrder.Asset.ID,
+			Price:         t.Price,
+			Shares:        t.SellingOrder.Shares - t.SellingOrder.PendingShares,
+		}
+		transactionsOutput = append(transactionsOutput, transactionsOutput)
+	}
+	output.TransactionOutput = transactionsOutput
+	return output
+}
