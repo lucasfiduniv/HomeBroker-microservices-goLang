17daf30a7731f58e08d5115e5693a20670517f8e
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2023-12-12 10:24:38 -0300

    antes de atualizar alguns codigo s

--- cmd/trade/main.go
+++ cmd/trade/main.go
@@ -13,39 +13,44 @@ import (
 )
 
 func main() {
-	orderIn := make(chan *entity.Order)
-	orderOut := make(chan *entity.Order)
+	ordersIn := make(chan *entity.Order)
+	ordersOut := make(chan *entity.Order)
 	wg := &sync.WaitGroup{}
 	defer wg.Wait()
 
-	kafkaMsgCha := make(chan *ckafka.Message)
+	kafkaMsgChan := make(chan *ckafka.Message)
 	configMap := &ckafka.ConfigMap{
-		"bootstrap.server":  "host.docker.internal:9094",
-		"group.id":          "mygroup",
-		"auto.offset.reset": "earliest",
+		"bootstrap.servers": "host.docker.internal:9094",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "latest",
 	}
 	producer := kafka.NewKafkaProducer(configMap)
 	kafka := kafka.NewConsumer(configMap, []string{"input"})
 
-	go kafka.Consume(kafkaMsgCha)
+	go kafka.Consume(kafkaMsgChan) // T2
 
-	book := entity.NewBook(orderIn, orderOut, wg)
-	go book.Trade()
+	// recebe do canal do kafka, joga no input, processa joga no output e depois publica no kafka
+	book := entity.NewBook(ordersIn, ordersOut, wg)
+	go book.Trade() // T3
 
 	go func() {
 		for msg := range kafkaMsgChan {
+			wg.Add(1)
+			fmt.Println(string(msg.Value))
 			tradeInput := dto.TradeInput{}
 			err := json.Unmarshal(msg.Value, &tradeInput)
 			if err != nil {
 				panic(err)
 			}
 			order := transformer.TransformerInput(tradeInput)
-			orderIn <- order
+			ordersIn <- order
 		}
 	}()
+
 	for res := range ordersOut {
 		output := transformer.TranformOutput(res)
-		outputJson, err := json.MarshalIndent(output, "", "\t")
+		outputJson, err := json.MarshalIndent(output, "", "  ")
+		fmt.Println(string(outputJson))
 		if err != nil {
 			fmt.Println(err)
 		}
